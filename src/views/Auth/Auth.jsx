59461aea4019c05437b3181617ea9f662ee950aa
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2022-01-05 14:13:37 -0800

    fixed id

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,22 +1,33 @@
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import UserForm from '../../components/UserForm/UserForm';
+import { useState } from 'react';
 import { useUser } from '../../context/UserContext';
 import { signInUser, signUpUser } from '../../services/users';
 
 import styles from './Auth.css';
 
+// TODO: Implement sign up & sign
+// Use isSigningUp to determine whether to sign up or sign in a user
+// If signing in: set the user ({id, email}) and redirect to /notes
+// If signing up: redirect to /confirm-email
+// Use the corresponding functions from `/services/users` for both cases
+
 export default function Auth({ isSigningUp = false }) {
   const history = useHistory();
   const { setUser } = useUser();
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (email, password) => {
     try {
-      // TODO: Implement sign up & sign
-      // Use isSigningUp to determine whether to sign up or sign in a user
-      // If signing in: set the user ({id, email}) and redirect to /notes
-      // If signing up: redirect to /confirm-email
-      // Use the corresponding functions from `/services/users` for both cases
+      if (isSigningUp) {
+        signUpUser(email, password);
+        history.replace('/confirm-email');
+      } else {
+        const user = await signInUser(email, password);
+        setUser({ id: user.id, email });
+        history.replace('/notes');
+      }
     } catch (error) {
       throw error;
     }
@@ -31,7 +42,7 @@ export default function Auth({ isSigningUp = false }) {
         onSubmit={handleSubmit}
         label={isSigningUp ? 'Sign Up' : 'Sign In'}
       />
-
+      {error ? <p>{error}</p> : <></>}
       {isSigningUp ? (
         <p>
           Already have an account? <Link to="/login">Sign In</Link>
